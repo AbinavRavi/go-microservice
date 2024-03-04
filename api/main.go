48e4ae99d17a347fa2cfa1177cc4ca3b3b4efbe8
AbinavRavi <subramathreya@gmail.com>
2024-03-04 12:27:12 +0530

    the jwt creator and authentication works, change readme

--- api/main.go
+++ api/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 	"log"
 	"net/http"
 	"os"
@@ -14,17 +14,43 @@ func homepage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Secret Information, Uncle SAM is watching you")
 }
 
-func isAuthorized(w http.ResponseWriter, r *http.Request){
-	return http.HandleFunc(func( w http.ResponseWriter, r *http.Request){
-		if r.Header["Token"] != nil{
+func isAuthorized(endpoint func(w http.ResponseWriter, r *http.Request)) http.Handler{
+	return http.HandlerFunc(func ( w http.ResponseWriter, r *http.Request){
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func( token *jwt.Token) (interface{}, error){
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC);!ok{
+					return nil, fmt.Errorf("Invalid signing method")
+				}
+				aud := "billing.jwtgo.io"
+				checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+				if !checkAudience{
+					return nil, fmt.Errorf("invalid audience")
+				}
+				iss := "jwtgo.io"
+				checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+				if !checkIss{
+					return nil, fmt.Errorf("Invalid Issuer")
+				}
+				return MySigninKey, nil
+			})
+
+			if err != nil{
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid{
+				endpoint(w,r)
+			}
 			
+		}else {
+			fmt.Fprintf(w, "No Authorization token provided")
 		}
 	})
 }
 
 func handleRequests(){
 	http.Handle("/",isAuthorized(homepage))
-	log.Fatal(http.ListenAndServe("9001",nil))
+	log.Fatal(http.ListenAndServe(":9001",nil))
 
 }
 
--- jwt_creator/main.go
+++ jwt_creator/main.go
@@ -9,7 +9,42 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
+var MySigninKey = []byte(os.Getenv("SECRET_KEY"))
+
+func GetJWT() (string, error){
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["User"] = "Abinav"
+	claims["aud"] = "billing.jwtgo.io"
+	claims["iss"] = "jwtgo.io"
+	claims["exp"] = time.Now().Add(time.Minute*5).Unix()
+
+	tokenString, err := token.SignedString(MySigninKey)
+	if err != nil {
+		fmt.Errorf("Something went wrong: %s", err)
+	}
+
+	return tokenString, nil
+}
+
+func Index(w http.ResponseWriter, r *http.Request){
+	validToken, err := GetJWT()
+	fmt.Println(validToken)
+	if err != nil {
+		fmt.Println("Failed to generate token")
+	}
+	fmt.Fprintf(w, string(validToken))
+}
+
+func handleRequests(){
+	http.HandleFunc("/", Index)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
 func main(){
-	
+	handleRequests()
 }
 
