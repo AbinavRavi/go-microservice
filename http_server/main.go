15e9046675d411e7e2bdd2cda8076700fba821f7
AbinavRavi <subramathreya@gmail.com>
2024-03-08 18:51:24 +0530

    added a simple http server

--- http_server/main.go
+++ http_server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
+func formHandler(writer http.ResponseWriter, req *http.Request){
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(writer, "ParseForm() err : %v", err)
+		return
+	}
+	fmt.Fprintf(writer,"POST request successful")
+	name := req.FormValue("name")
+	fmt.Fprintf(writer, "Name = %s \n", name)
+}
 
 func main(){
-	fmt.Println("hello world server")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
